fd9aefa7424a89cfdd130e21b2b369ba22ae690a
Nicole <lancasternj@gmail.com>
2023-04-19 16:21:51 +0100

    rendering error for user if unable to get comments and message for when article contains no comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,44 +2,46 @@ import { fetchCommentsByArticleID } from "../api";
 import { useEffect, useState } from "react";
 import { sqlDateFormatter } from "../utils.js";
 
-
 const Comments = ({ article_id, comments, setComments }) => {
-  const [isLoading, setIsLoading] = useState(true)
-  const [isError, setIsError] = useState(false)
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
     if (typeof article_id !== "undefined" && article_id !== "") {
       fetchCommentsByArticleID(article_id)
         .then((comments) => {
           setComments(comments);
-          setIsLoading(false)
+          setIsLoading(false);
         })
         .catch(() => {
-      setIsError(true) 
-        })
+          setIsError(true);
+          setIsLoading(false);
+        });
     }
   }, [article_id, setComments]);
 
-  if (isError) return <p>Unable to load comments</p>
-  if (isLoading) return <p>Loading comments...</p>
+  if (isError) return <p>Sorry, we are unable to load comments at the moment</p>;
+  if (isLoading) return <p>Loading comments...</p>;
 
   return (
     <section className="Comments">
       <h3>Comments</h3>
-      {comments.comment_count=== 0 ? "No comments - be the first to post one!" : null }
       <ul className="comments-flex-container">
-        {comments.map((comment) => {
-          return (
-            <li key={comment.comment_id} className="comment-flex-item">
-              <p>{comment.body}</p>
-              <p className="comment-author-date">
-                posted by {comment.author} at {sqlDateFormatter(comment.created_at)}
-              </p>
-              <p>{comment.votes} likes</p>
-            </li>
-          );
-        })}
+        {comments.length === 0
+          ? "No comments - be the first to post one!"
+          : comments.map((comment) => {
+              return (
+                <li key={comment.comment_id} className="comment-flex-item">
+                  <p>{comment.body}</p>
+                  <p className="comment-author-date">
+                    posted by {comment.author} at{" "}
+                    {sqlDateFormatter(comment.created_at)}
+                  </p>
+                  <p>{comment.votes} likes</p>
+                </li>
+              );
+            })}
       </ul>
     </section>
   );
