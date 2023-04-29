3bc614a9ff79f7d631ebb259ff58f087621c5353
Nicole <lancasternj@gmail.com>
2023-04-29 15:34:47 +0100

    removing container in navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import UserLogin from "./UserLogin.jsx";
 import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 
 const NavBar = ({ currentUser, users, setCurrentUser }) => {
@@ -18,35 +17,33 @@ const NavBar = ({ currentUser, users, setCurrentUser }) => {
   );
   return (
     <Navbar bg="light" expand="lg" className="nav-bar">
-      <Container>
-        <Navbar.Brand to="/" className="nav-bar-brand">
-          Nicole's NC-News
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Item>
-              <Link className="nav-links" to="/">
-                Home
-              </Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link className="nav-links" to="/users">
-                Users
-              </Link>
-            </Nav.Item>
-            <Nav.Item>
-              {" "}
-              <UserLogin currentUser={currentUser} users={users} />{" "}
-              {!currentUser ? (
-                <Link to={`/users`}>{loginBtn}</Link>
-              ) : (
-                <>{loginBtn}</>
-              )}
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+      <Navbar.Brand to="/" className="nav-bar-brand">
+        Nicole's NC-News
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Item>
+            <Link className="nav-links" to="/">
+              Home
+            </Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link className="nav-links" to="/users">
+              Users
+            </Link>
+          </Nav.Item>
+          <Nav.Item>
+            {" "}
+            <UserLogin currentUser={currentUser} users={users} />{" "}
+            {!currentUser ? (
+              <Link to={`/users`}>{loginBtn}</Link>
+            ) : (
+              <>{loginBtn}</>
+            )}
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
