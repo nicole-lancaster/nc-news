3b0ee55e6e6689a64aecf279a4a60ca1c5749c41
Nicole <lancasternj@gmail.com>
2023-05-06 20:18:04 +0100

    adding tailwind icons for home and users nav links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import UserLogin from "./UserLogin.jsx";
-import img from "../assets/speechbubble.png"
+import { HomeIcon, UsersIcon } from "@heroicons/react/24/outline";
 
 const NavBar = ({ currentUser, users, setCurrentUser }) => {
   const handleLoginClick = () => {
@@ -17,26 +17,17 @@ const NavBar = ({ currentUser, users, setCurrentUser }) => {
   return (
     <header>
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
-        <div className="flex lg:flex-1">
-          {/* <a href="#" className="-m-1.5 p-1.5"> */}
-          {/* <span className="sr-only">Your Company</span> */}
-          <img
-            className="h-11 w-auto"
-            src={img}
-            alt="Nicoles News logo"
-          />
-          <h1 className="text-3xl font-bold">Nicole's NC-News</h1>
-
-          {/* </a> */}
-        </div>
+        <h1 className="text-3xl font-bold">Nicole's NC-News</h1>
         <ul>
           <li>
             <Link className="nav-links" to="/">
+              <HomeIcon className="h-8 w-8 text-black-500" />
               Home
             </Link>
           </li>
           <li>
             <Link className="nav-links" to="/users">
+              <UsersIcon className="h-8 w-8 text-black-500" />
               Users
             </Link>
           </li>
