dbb2382f019a5daff748cc72c2b6234fa1a80fa9
Nicole <lancasternj@gmail.com>
2023-05-08 14:18:43 +0100

    adding like button to article page and styling on hover over users

--- src/components/SingleUser.jsx
+++ src/components/SingleUser.jsx
@@ -12,22 +12,21 @@ const SingleUser = ({ user, currentUser, setCurrentUser }) => {
   if (isLoading) return <p>Logging in...</p>;
 
   return (
-    <div className="single-user">
-      <li className="single-user-name">
-        {user.name}
-        <p className="single-user-username">{user.username}</p>
-        <img
-          className="user-list-avatar"
-          src={user.avatar_url}
-          alt={user.username}
-        />
-      </li>
+    <div className="single-user ">
       <button
-        className="user-pretend-to-be-btn"
+        className="user-pretend-to-be-btn border border-white bg-none hover:border-black"
         onClick={() => handleLoginClick(user)}
         disabled={user.username === currentUser?.username}
       >
-        Pretend to be {user.name}
+        <li className="single-user-name">
+          {user.name}
+          <p className="single-user-username">{user.username}</p>
+          <img
+            className="user-list-avatar"
+            src={user.avatar_url}
+            alt={user.username}
+          />
+        </li>
       </button>
       {currentUser === user ? (
         <p className="user-logged-in-as-msg">
