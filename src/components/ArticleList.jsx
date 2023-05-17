67fc8149eb69bbd0a2b004eba1fe06cc3a3a2d54
Nicole <lancasternj@gmail.com>
2023-05-17 11:49:40 +0100

    making article title and info responsive and fit to article img

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -34,17 +34,18 @@ const ArticleList = ({ articles, setArticles, selectedTopic }) => {
                 key={article.article_id}
                 to={`/articles/${article.article_id}`}
               >
-                <li>
+                <li className="w-32 md:w-48 lg:w-60 xl:w-100 2xl:w-110">
                   <img
-                    className="w-32 md:w-48 lg:w-60"
                     src={article.article_img_url}
                     alt={article.title}
                   />
-                  <p> {article.title}</p>
-                  <p>
-                    Written by {article.author} on
+                  <div>
+                  <p className="text-sm md:text-base lg:text-lg"> {article.title}</p>
+                  <p className="text-xs md:text-sm lg:text-base italic">
+                    Written by {article.author} on 
                     {sqlDateFormatter(article.created_at)}
                   </p>
+                  </div>
                 </li>
               </Link>
             );
