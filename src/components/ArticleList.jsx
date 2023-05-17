2b39fb31f80822bb59d38248f4d2d1ce7971d386
Nicole <lancasternj@gmail.com>
2023-05-17 12:42:35 +0100

    adding flex props to article list items and removing author and date

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchArticles } from "../api.js";
 import { Link } from "react-router-dom";
-import { sqlDateFormatter } from "../utils.js";
 
 const ArticleList = ({ articles, setArticles, selectedTopic }) => {
   const [isLoading, setIsLoading] = useState(true);
@@ -23,7 +22,7 @@ const ArticleList = ({ articles, setArticles, selectedTopic }) => {
 
   return (
     <section>
-      <ul>
+      <ul className="flex flex-row flex-wrap justify-evenly">
         {articles
           .filter(
             (article) => !selectedTopic || article.topic === selectedTopic
@@ -39,13 +38,7 @@ const ArticleList = ({ articles, setArticles, selectedTopic }) => {
                     src={article.article_img_url}
                     alt={article.title}
                   />
-                  <div>
-                  <p className="text-sm md:text-base lg:text-lg"> {article.title}</p>
-                  <p className="text-xs md:text-sm lg:text-base italic">
-                    Written by {article.author} on 
-                    {sqlDateFormatter(article.created_at)}
-                  </p>
-                  </div>
+                  <p className="text-sm md:text-base lg:text-lg xl:text-xl" > {article.title}</p>
                 </li>
               </Link>
             );
