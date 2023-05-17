d848191be33e4e41a83e107010e09bd03aa3090c
Nicole <lancasternj@gmail.com>
2023-05-17 15:39:59 +0100

    adding styling and pulse loading to article list's loading message

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { fetchArticles } from "../api.js";
 import { Link } from "react-router-dom";
+import { NewspaperIcon } from "@heroicons/react/24/outline";
 
 const ArticleList = ({ articles, setArticles, selectedTopic }) => {
   const [isLoading, setIsLoading] = useState(true);
@@ -15,7 +16,8 @@ const ArticleList = ({ articles, setArticles, selectedTopic }) => {
 
   if (isLoading)
     return (
-      <div>
+      <div className="flex flex-col h-screen justify-center items-center animate-pulse">
+        <NewspaperIcon className="h-8 w-auto mr-2 animate-pulse" />
         <p className="font-mono">Loading list of articles...</p>
       </div>
     );
@@ -35,7 +37,7 @@ const ArticleList = ({ articles, setArticles, selectedTopic }) => {
               >
                 <li className="content-around mx-1 my-2 w-32 md:w-48 lg:w-60 xl:w-100 2xl:w-110">
                   <img src={article.article_img_url} alt={article.title} />
-                  <p className="text-center text-sm md:text-base lg:text-lg xl:text-xl font-mono">
+                  <p className="text-center text-xs md:text-sm lg:text-base xl:text-lg font-mono mt-2">
                     {article.title}
                   </p>
                 </li>
