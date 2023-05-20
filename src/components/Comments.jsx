16204e0f0ffebfd5705e20ae46690cca13a43cb7
Nicole <lancasternj@gmail.com>
2023-05-20 12:47:24 +0100

    adding styling to comments section

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -63,11 +63,11 @@ const Comments = ({
     );
 
   return (
-    <section>
-      <h3 className="font-mono m-5 font-bold">Comments</h3>
+    <section className="flex flex-col items-center m-5 p-5 w-auto md:w-3/4 xl:w-1/2">
+      <h3 className="font-mono text-base 2xl:text-xl m-5 font-bold">Comments</h3>
       <form
         onSubmit={handleCommentSubmit}
-        className="flex flex-col p-5 m-5 border-2 border-pink-500 shadow rounded-lg"
+        className="flex flex-col p-5 m-5 border-2 border-pink-500 shadow rounded-lg w-auto md:w-3/4 xl:w-1/2 "
       >
         <label className="font-mono" htmlFor="comment-input-box"></label>
         <textarea
@@ -85,13 +85,13 @@ const Comments = ({
         </button>
       </form>
       {isCommentLoading && currentUser ? (
-        <p className="font-mono">Posting comment...</p>
+        <p className="font-mono text-center font-bold">Posting comment...</p>
       ) : null}
       {hasPosted && currentUser ? (
-        <p className="font-mono">Comment added!</p>
+        <p className="font-mono text-center font-bold">Comment added!</p>
       ) : null}{" "}
       {isPostingError && !currentUser ? (
-        <p className="font-mono">
+        <p className="font-mono font-bold text-center w-auto md:w-3/4 xl:w-1/2 ">
           Sorry, we are unable to post your comment. Check you are logged in
           then try again!
         </p>
@@ -103,14 +103,14 @@ const Comments = ({
               return (
                 <li
                   key={comment.comment_id}
-                  className="flex flex-col p-5 self-center m-5 border-2 border-black shadow rounded-lg "
+                  className="flex flex-col p-5 self-center m-5 border-2 border-black shadow rounded-lg w-auto md:w-3/4 xl:w-1/2 "
                 >
-                  <p className="font-mono italic">
+                  <p className="font-mono text-xs 2xl:text-base italic ml-1">
                     by {comment.author} at{" "}
                     {sqlDateFormatter(comment.created_at)}
                   </p>
-                  <p className="font-mono font-bold">{comment.body}</p>
-                  <p className="font-mono">{comment.votes} likes</p>
+                  <p className="font-mono text-sm 2xl:text-lg my-3">{comment.body}</p>
+                  <p className="font-mono font-bold text-xs 2xl:text-lg flex flex-row justify-end mr-1">{comment.votes} likes</p>
                 </li>
               );
             })}
