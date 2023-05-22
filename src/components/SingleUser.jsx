50773b9ccb8ac2ddab53c611b6a8e126bf1b6313
Nicole <lancasternj@gmail.com>
2023-05-22 10:59:18 +0100

    adding further styling to single user cards

--- src/components/SingleUser.jsx
+++ src/components/SingleUser.jsx
@@ -14,19 +14,18 @@ const SingleUser = ({ user, currentUser, setCurrentUser }) => {
 
   return (
     <section>
-      <div className="border-2 border-light-gray-700 shadow rounded-lg hover:border-pink-700 m-1 p-1 w-32 md:w-48 lg:m-2 2xl:w-110">
+      <div className="border-2 border-light-gray-700 shadow rounded-lg hover:border-pink-700 m-1 p-1 w-32 md:w-48 lg:m-2 xl:w-120">
         <button
-          className=""
+          type="submit"
           onClick={() => handleLoginClick(user)}
           disabled={user.username === currentUser?.username}
         >
-          <li className="flex flex-col items-center justify-center content-around mx-1 my-2 ">
-            
+          <li className="flex flex-col items-center mx-1 my-2 ">
             <p className="text-center text-xs md:text-sm lg:text-base xl:text-lg font-mono mt-2">
               {user.name} as {user.username}
             </p>
             <img
-              className="p-1 w-16 h-16 self-center"
+              className="p-1 w-fit h-20 self-center"
               src={user.avatar_url}
               alt={user.username}
             />
