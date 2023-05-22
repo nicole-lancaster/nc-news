66431b8cc43741b27a9aad12a03e41bd66476a5b
Nicole <lancasternj@gmail.com>
2023-05-22 10:21:51 +0100

    centering single users and ensuring users displaying are responsive on all device sizes

--- src/components/SingleUser.jsx
+++ src/components/SingleUser.jsx
@@ -14,19 +14,19 @@ const SingleUser = ({ user, currentUser, setCurrentUser }) => {
 
   return (
     <section>
-      <div className="border-2 border-light-gray-700 shadow rounded-lg hover:border-pink-700 m-1 p-1 w-32 md:w-48 lg:w-60 xl:w-100 2xl:w-110">
+      <div className="border-2 border-light-gray-700 shadow rounded-lg hover:border-pink-700 m-1 p-1 w-32 md:w-48 lg:m-2 2xl:w-110">
         <button
           className=""
           onClick={() => handleLoginClick(user)}
           disabled={user.username === currentUser?.username}
         >
-          <li className="content-around mx-1 my-2 ">
-            {user.name}
+          <li className="flex flex-col items-center justify-center content-around mx-1 my-2 ">
+            
             <p className="text-center text-xs md:text-sm lg:text-base xl:text-lg font-mono mt-2">
-              {user.username}
+              {user.name} as {user.username}
             </p>
             <img
-              className="p-1 w-20 h-20"
+              className="p-1 w-16 h-16 self-center"
               src={user.avatar_url}
               alt={user.username}
             />
