9a977cb6cb8751d72aa19bd1a018839f881c912d
Nicole <lancasternj@gmail.com>
2023-06-02 16:20:12 +0100

    testing that all links open in new tab on footer

--- src/tests/Footer.test.jsx
+++ src/tests/Footer.test.jsx
@@ -1,9 +1,18 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Footer from "../components/Footer";
 
-describe('Footer', () => {
-    test('should match rendered snapshot', () => {
-        const {asFragment} = render(<Footer/>)
-        expect(asFragment()).toMatchSnapshot()
-    })
-})
+describe("Footer", () => {
+  test("should match rendered snapshot", () => {
+    const { asFragment } = render(<Footer />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  test("should open links in new tab", async () => {
+    render(<Footer />);
+
+    const anchorTags = await screen.findAllByRole("link");
+    const doAllLinksOpenInNewTab = anchorTags
+      .map((tag) => tag.getAttribute("target"))
+      .every((targetAttribute) => targetAttribute === "_blank");
+    expect(doAllLinksOpenInNewTab).toBe(true);
+  });
+});
